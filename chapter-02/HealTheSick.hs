66c0adc20fea714a95ef1469bf25d356c1f55082
Joan Llenas <joan.llenas.maso@gmail.com>
2019-02-07 08:36:25 +0100

    Chapter 2 complete

---
+++ chapter-02/HealTheSick.hs
@@ -0,0 +1,13 @@
+module HealTheSick where
+
+{- Heal the sick p.67 -}
+-- 1.
+area x = 3.14 * (x * x) -- space between `3.` and `14`
+
+-- 2.
+double x = x * 2 -- `b` did not exists
+
+-- 3.
+x = 7
+y = 10 -- wrong indentation
+f = x + y
